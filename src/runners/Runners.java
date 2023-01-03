04f3079ed8117ec2c743c1d5ebaa2ecc37905656
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-03 14:32:56 -0600

    creating runners

--- src/runners/Runners.java
+++ src/runners/Runners.java
@@ -1,4 +1,10 @@
 package runners;
 
+import filesSummarise.IfFileSummarise;
+import utils.readUtils;
+
 public class Runners {
+    private readUtils m_readUtils;
+    private IfFileSummarise m_ifFileSummarise;
+
 }
