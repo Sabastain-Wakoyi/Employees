fd5438f94b8e0a309c59cb5b7b8bbb742ad5ce32
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-03 14:51:50 -0600

    creating runners

--- src/runners/Runners.java
+++ src/runners/Runners.java
@@ -3,8 +3,22 @@ package runners;
 import filesSummarise.IfFileSummarise;
 import utils.readUtils;
 
-public class Runners {
+public class Runners<List, Employees> {
     private readUtils m_readUtils;
     private IfFileSummarise m_ifFileSummarise;
 
+    public Runners(){
+        m_readUtils = new readUtils();
+        m_ifFileSummarise = new IfFileSummarise(){};
+
+        UserOptions();
+
+        }
+
+    private  void UserOptions() {
+        int choice = 0;
+        List<Employees> employeeslist = new ArrayList<Employees>();
+    }
+
 }
+
