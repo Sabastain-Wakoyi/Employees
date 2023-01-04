35bffd16d37e3ca873d2912b702a53ab2e1e2c6e
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-03 22:42:31 -0600

    call summary

--- src/filesummaryservice/FileSummaryServiceImpl.java
+++ src/filesummaryservice/FileSummaryServiceImpl.java
@@ -64,13 +64,36 @@ package filesummaryservice;
 
 
 import bean.callSummary;
+import util.ReadUtil;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileSummaryServiceImpl implements IFileSummaryService{
 
+    ReadUtil readUtil;
+
+    public FileSummaryServiceImpl(){
+        readUtil = new ReadUtil();
+    }
+
+
     @Override
-    public List<callSummary> getCallSummary(String filePath) {
-        return null;
+    public List<callSummary> getCallSummary(String filePath) throws IOException {
+
+        List<callSummary> callSummaryList = new ArrayList<>();
+        List<String> filteredLines = new ArrayList<>();
+        List<String> allLines = Files.readAllLines(Paths.get(path.getAbsolutePath()), Charset.defaultCharset());
+        allLines.forEach(x->{
+            callSummaryList summary = buildCallSummary(x);
+            if(summary!=null){
+                callSummaryList.add(callSummary);
+            }
+        });
+        return callSummaryList;
     }
 }
